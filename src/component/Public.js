d4ee653135b047a2a78eb3c1aa0c4caf98c290eb
SEN <millionworlds2190@gmail.com>
2023-03-14 09:55:58 +0530

    ui updates

--- src/component/Public.js
+++ src/component/Public.js
@@ -6,30 +6,28 @@ const Public = () => {
         <>
             {/* content */}
             <div>
-                <div>
-                    <img src="./imgs/img (1).webp" width="400em" style={{ 'margin': '16px 20px' }} alt='resort' />
-                    <p style={{ 'text-align': 'center', fontSize: 'small' }} >
-                        location 1
-                    </p>
-                </div>
-                <div style={{ 'min-width': '10em' }}>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vel dui pellentesque, elementum eros
-                        sit amet, rutrum mauris. Nunc ornare nulla non mauris pharetra ornare. Mauris at est ac sem commodo
-                        efficitur. Nullam imperdiet magna non commodo ullamcorper. Curabitur maximus orci iaculis massa
-                        interdum varius. Maecenas non nisl tortor. Sed vel ornare erat.
+                <img src="./imgs/img (1).webp" style={{ 'text-align': 'center' }} alt='resort' />
+                <p style={{ 'text-align': 'center', fontSize: 'small' }} >
+                    location 1
+                </p>
+            </div>
+            <div style={{ 'min-width': '10em' }}>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vel dui pellentesque, elementum eros
+                    sit amet, rutrum mauris. Nunc ornare nulla non mauris pharetra ornare. Mauris at est ac sem commodo
+                    efficitur. Nullam imperdiet magna non commodo ullamcorper. Curabitur maximus orci iaculis massa
+                    interdum varius. Maecenas non nisl tortor. Sed vel ornare erat.
 
-                        Vivamus nec porta quam. Fusce ut lectus in nisl pretium auctor id id purus. Nullam tellus sapien,
-                        tempus ac pellentesque sed, pharetra ut libero. Quisque facilisis semper ante, at cursus orci
-                        vehicula
-                        nec. Ut sagittis sollicitudin est, ac ultricies leo vehicula ac. Maecenas in fringilla eros, vel
-                        dignissim risus. Donec facilisis ornare tortor vitae eleifend. Nam eu facilisis libero. Sed dapibus
-                        condimentum diam in tempus. Nam pharetra arcu enim, a venenatis justo dictum et. Pellentesque
-                        rhoncus
-                        elit at faucibus ullamcorper. Aenean nec nunc convallis nulla vulputate rutrum et in nibh. Praesent
-                        elit ex, cursus et venenatis sit amet, varius luctus erat. Pellentesque nec velit et metus rhoncus
-                        tempus. Duis faucibus pulvinar est, id accumsan risus fringilla id.
-                    </p>
-                </div>
+                    Vivamus nec porta quam. Fusce ut lectus in nisl pretium auctor id id purus. Nullam tellus sapien,
+                    tempus ac pellentesque sed, pharetra ut libero. Quisque facilisis semper ante, at cursus orci
+                    vehicula
+                    nec. Ut sagittis sollicitudin est, ac ultricies leo vehicula ac. Maecenas in fringilla eros, vel
+                    dignissim risus. Donec facilisis ornare tortor vitae eleifend. Nam eu facilisis libero. Sed dapibus
+                    condimentum diam in tempus. Nam pharetra arcu enim, a venenatis justo dictum et. Pellentesque
+                    rhoncus
+                    elit at faucibus ullamcorper. Aenean nec nunc convallis nulla vulputate rutrum et in nibh. Praesent
+                    elit ex, cursus et venenatis sit amet, varius luctus erat. Pellentesque nec velit et metus rhoncus
+                    tempus. Duis faucibus pulvinar est, id accumsan risus fringilla id.
+                </p>
             </div>
             <div style={{
                 backgroundColor: 'var(--theme-color)',
